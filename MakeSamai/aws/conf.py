f81b2470fdd99ae0cc6ca90e9faaa07eb932c43e
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-02-26 16:33:51 +0530

    Initial Commit

---
+++ MakeSamai/aws/conf.py
@@ -0,0 +1,31 @@
+import os
+import datetime
+
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_S3_REGION_NAME = os.environ['S3_REGION_NAME']
+AWS_FILE_EXPIRE = 200
+AWS_PRELOAD_METADATA = True
+AWS_QUERYSTRING_AUTH = True
+S3_USE_SIGV4 = True
+
+DEFAULT_FILE_STORAGE = 'Samai.aws.utils.MediaRootS3BotoStorage'
+STATICFILES_STORAGE = 'Samai.aws.utils.StaticRootS3BotoStorage'
+S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = MEDIA_URL
+STATIC_URL = S3_URL + 'static/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+two_months = datetime.timedelta(days=61)
+date_two_months_later = datetime.date.today() + two_months
+expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+
+AWS_HEADERS = {
+
+    'Expires': expires,
+    'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()),),
+}
+
+AWS_S3_SIGNATURE_VERSION = 's3v4'
