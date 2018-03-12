95fa0c1b81cf65cb9f6dc31af63f8c2706c47d23
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-03-12 19:22:27 +0530

    Updated settings.py

--- MakeSamai/settings.py
+++ MakeSamai/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 from .aws.conf import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -179,3 +180,5 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+django_heroku.settings(locals())
