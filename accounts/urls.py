a1b744e491d703dd72ccba4b1fa5268e54069cd5
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-03-22 18:15:35 +0530

    Account List Endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('api/account/create/', views.AccountCreate.as_view(), name='create'),
-    path('api/account/u/<slug:user>/', views.UserAccountList.as_view(), name='list'),
+    path('api/account/', views.UserAccountList.as_view(), name='list'),
     path('api/account/<slug:uaid>/', views.AccountRetrieve.as_view(), name='retrieve'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -22,11 +22,13 @@ class AccountRetrieve(generics.RetrieveUpdateDestroyAPIView):
 
 class UserAccountList(generics.ListAPIView):
     serializer_class = serializers.AccountSerializer
+    user = None
 
     def get_queryset(self):
         """
         This view should return a list of all the accounts for
         the user as determined by the user portion of the URL.
         """
-        user = self.kwargs['user']
-        return models.Account.objects.user_accounts(user=user).order_by('-created_on')
+        if self.request and hasattr(self.request, "user"):
+            self.user = self.request.user
+        return models.Account.objects.user_accounts(user=self.user).order_by('-created_on')
