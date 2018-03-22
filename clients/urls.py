809c9ca6bcacf26584f6d13daaac1cf0f3967140
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-03-22 16:02:10 +0530

    Me API

--- clients/urls.py
+++ clients/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'clients'
 urlpatterns = [
     path('api/signup/', views.CreateClient.as_view(), name='create'),
+    path('api/me/', views.UserView.as_view(), name='me'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,5 +1,5 @@
-from rest_framework import status
-from rest_framework.permissions import AllowAny
+from django.conf import settings
+from rest_framework import status, generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -10,7 +10,7 @@ class CreateClient(APIView):
     """
     Creates the user.
     """
-    permission_classes = (AllowAny,)
+    permission_classes = (permissions.AllowAny,)
 
     def post(self, request, format='json'):
         serializer = serializers.ClientSerializer(data=request.data)
@@ -19,3 +19,15 @@ class CreateClient(APIView):
             if user:
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserView(generics.RetrieveUpdateAPIView):
+    """
+    Use this endpoint to retrieve/update user.
+    """
+    model = settings.AUTH_USER_MODEL
+    serializer_class = serializers.ClientSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self, *args, **kwargs):
+        return self.request.user
