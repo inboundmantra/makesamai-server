38686b32bcb928c1024d05e0082dbc161221a803
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-03-26 00:04:13 +0530

    Migrations

--- landing_pages/models.py
+++ landing_pages/models.py
@@ -13,14 +13,14 @@ class LandingPage(models.Model):
     """
     ulid = RandomSlugField(length=7, unique=True, primary_key=True)
     account = models.ForeignKey(Account, on_delete=models.CASCADE, to_field='uaid')
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, to_field='username', related_name='lp_created_by',
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, to_field='uuid', related_name='lp_created_by',
                                    on_delete=models.CASCADE)
     title = models.CharField(_('Title'), max_length=255)
     slug = models.SlugField()
     description = models.TextField(_('Description'), blank=True, null=True)
     created_on = models.DateTimeField(_("Created On"), auto_now_add=True)
     brand_name = models.CharField(_('Brand Name'), max_length=255, default='XYZ Company')
-    form = models.ForeignKey(Form, to_field='title', related_name='lp_form')
+    form = models.ForeignKey(Form, on_delete=models.CASCADE, to_field='ufid', related_name='lp_form')
     thank_you_message = models.TextField(default="Your Response Has Been Recorded, Thank You For Submitting the Form!")
 
     class Meta:
