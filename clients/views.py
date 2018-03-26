304e2ef817fca447d6c100ba9668f7103eef23e2
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-03-26 12:09:09 +0530

    Welcome Mail

--- clients/views.py
+++ clients/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.mail import send_mail
 from rest_framework import status, generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,6 +18,14 @@ class CreateClient(APIView):
         if serializer.is_valid():
             user = serializer.save()
             if user:
+                send_mail(
+                    "Welcome To Make Samai!",
+                    "Hi " + user.first_name + ",\n We are glad to have you onboard Make Samai. Get Started With Superpowered Marketing Today.",
+                    'notification-noreply@makesamai.com',
+                    [user.email],
+                    fail_silently=True,
+                    html_message="Hi " + user.first_name + ",<br> We are glad to have you onboard <strong>MakeSamai</strong>. Get Started With Superpowered Marketing Today.<br><br>Regards,<br>Team MakeSamai",
+                )
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
